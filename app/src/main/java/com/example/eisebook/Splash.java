bc65b1ec00c7ee258cbeb4d98ed74e72c88d2c43
Varshar27 <varsharanganath27@gmail.com>
2019-08-07 19:50:51 +0100

    Splash screen

---
+++ app/src/main/java/com/example/eisebook/Splash.java
@@ -0,0 +1,33 @@
+package com.example.eisebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try
+                {
+                    Thread.sleep(1500);
+
+                    startActivity(new Intent(Splash.this,Login.class));
+
+                }catch (Exception e)
+                {
+
+                }
+            }
+        }).start();
+
+    }
+}
