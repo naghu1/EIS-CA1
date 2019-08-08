950f93fc28e9367c072a390f86466e768dab8739
Naghma Khan <naghu.sona@gmail.com>
2019-08-08 09:30:05 +0100

    Navigation Screen Design

--- app/src/main/java/com/example/eisebook/Navigation.java
+++ app/src/main/java/com/example/eisebook/Navigation.java
@@ -1,14 +1,23 @@
 package com.example.eisebook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Navigation extends AppCompatActivity {
+
+    private androidx.appcompat.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setTitle("EIS Ebooks");
+        setSupportActionBar(toolbar);
     }
 }
